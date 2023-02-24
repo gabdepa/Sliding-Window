3e0d3a70622afda37c49b15cc7c87a6f749aa422
gabdepa <gabriel-r-p-p@hotmail.com>
2023-02-24 19:50:46 -0300

    Adding comments

--- BackN/client_BackN.py
+++ BackN/client_BackN.py
@@ -2,105 +2,94 @@ import socket
 import sys
 import time
 
-# Tamanho do buffer
+# Buffer Size
 BUFFER_SIZE = 128
 
-# Endereço e porta do servidor
-SERVER_ADDRESS = ('localhost', 5000)
+SERVER_ADDRESS = ('localhost', 5000) # Port and address of server
 
-# Criação do socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Socket Creation
 
 while True:
     try:
-        # Tenta se conectar ao servidor
         sock.connect(SERVER_ADDRESS)
         print("=> Connection Established\n")
         break
     except ConnectionRefusedError:
-        # Caso a conexão seja recusada pelo servidor, termina o loop e encerra o programa
         print("=> Connection refused by server")
         sys.exit(1)
     except BlockingIOError:
-        # Se ainda não for possível conectar, espera 1 segundo e tenta novamente
         time.sleep(1)
         pass
     except ConnectionResetError:
         print("=> Connection closed by server")
 
-MAX_RETRIES = 3
-TIMEOUT = 30  # 30 segundos, meio minuto
-window_size = 3
-max_sequence = 7
-last_seq_num = seq_num = -1
-start_time = time.time()  # tempo de início do envio dos pacotes
-retries = 0
-data_size = 0
-window_data = [] # List that hold the window data
-flag = True
+MAX_RETRIES = 3 # Maximum number of retries
+TIMEOUT = 30  # 30 seconds
+window_size = 3 # Size of the window
+last_seq_num = seq_num = -1 # Initialization
+start_time = time.time()  # Time of sending the window
+retries = 0 # Initialization
+data_size = 0 # Initialization
+window_data = [] # List that hold the packets to send
+flag = True # Auxiliar flag
 
-# Abertura do arquivo
-filename = "./testMessage.txt"
+filename = "./testMessage.txt" # Input file
 with open(filename, 'rb') as f:
-    while ((time.time() - start_time) < TIMEOUT) and flag == True :  # Enquanto houver dados a serem enviados e o tempo limite não foi atingido
-        for i in range(window_size):
-            data = str(f.read(BUFFER_SIZE))
-            if (data == "b''"):
-                print("\nWINDOW PACKETS: ",window_data)
+    while ((time.time() - start_time) < TIMEOUT) and flag == True :  # While there is data to be sent and time limit has not been reached
+        for i in range(window_size): # Iterate from 0 to 2
+            data = str(f.read(BUFFER_SIZE)) # Read the file until reaches BUFFER_SIZE as a string, and puts its result on "data"
+            if (data == "b''"): # If read empty string, there is nothing more to read
+                print("\nWINDOW PACKETS: ",window_data) # Print of the list of packets to be sent
                 break
             else:
-                seq_num = int(seq_num) + 1
+                seq_num = int(seq_num) + 1 # Increment the sequence number
                 packet = str(seq_num) + " - " + data # Creating packet with sequence number
-                packet = packet.encode()
-                print(f"\n=> Sending Sequence Number: {seq_num} , Element {i} of window, Sending Data:\n {seq_num} - {data}")
-                window_data.append(packet)            
-                data_size += len(packet)              
+                packet = packet.encode() # Transform into bytes
+                print(f"\n=> Sending Sequence Number: {seq_num} , Element {i} of window, Sending Data:\n {seq_num} - {data}") # Print of data in the window
+                window_data.append(packet) # Append the packet to window
+                data_size += len(packet) # Take its size and add to the total size sent
             
-        if window_data != []: # Window lack of packets, there is nothing more to send, go to else
+        if window_data != []: # Window still has packets to send
             for packet in window_data: # Send each packet in the window
                     try:
                         sock.send(packet) # Send the data
                         message = sock.recv(BUFFER_SIZE) # Store the server return message
                         if ":ACK".encode() in message: # Received ACK
-                            print("\n=> Confirmation Received: ", message.decode('utf-8'))
-                            last_seq_num = seq_num
-                            rec_seq_num = message.decode().split(':')[0]
-                            print(f"RECEIVED ACK: {rec_seq_num}")
-                            print("SEQUENCE_NUMBER = ", seq_num)
-                            for i in window_data:
-                                l = i.decode()
-                                if rec_seq_num in l.split(' - ')[0]:
-                                    window_data.remove(i) 
-                            retries = 0  # reseta contador de tentativas após sucesso
+                            print("\n=> Confirmation Received: ", message.decode('utf-8')) # Print of the confirmation
+                            last_seq_num = seq_num # Last sequence is updated
+                            rec_seq_num = message.decode().split(':')[0] # Take the received sequence number from message
+                            print(f"RECEIVED ACK: {rec_seq_num}") # Print of the received sequence number
+                            for i in window_data: # Iterate over the window packets
+                                l = i.decode() # Decode the packet to see it's sequence number
+                                if rec_seq_num in l.split(' - ')[0]: # If the number received in in the sequence number of the packet
+                                    window_data.remove(i) # Here we take the packet that was ACK to server from the window
+                            retries = 0  # Reset after success the retries
                             print("\n----------WINDOW PACKETS AFTER ACK: ",window_data, "\n")
-                            break # Confirmação recebida, interrompe o loop
+                            break # Ack received, break the loop
                         else: # Received NACK
-                            print("==> Receipt not confirmed: ", message.decode('utf-8'))
-                            rec_seq_num = message.decode().split(':')[0]
-                            print(rec_seq_num)
-                            for i in window_data:
-                                seq_num = i.decode().split(' - ')[0]
-                                if seq_num == rec_seq_num:
-                                    sock.send(i) 
-                            retries += 0  # reseta contador de tentativas após sucesso
+                            retries += 1 # Increase the retrie of already sent packets
+                            print("==> Receipt not confirmed: ", message.decode('utf-8')) # Print of NACK Received
+                            rec_seq_num = message.decode().split(':')[0] # Take the received sequence number from message
+                            for i in window_data: # Iterate over the window packets
+                                seq_num = i.decode().split(' - ')[0] # Take the sequence number of the packet
+                                if seq_num == rec_seq_num: # If it matches with the packet
+                                    sock.send(i) # Send the packet again
                             print("\n---------WINDOW PACKETS AFTER NACK: ",window_data, "\n")
-                    except socket.timeout:
+                    except socket.timeout: # In case of timeout
                         retries += 1  # incrementa contador de tentativas
-                        print(f"\nTimeout reached ({retries}/{MAX_RETRIES}), resending packet...") # Timeout atingido, reenvia o pacote
-                        if retries > MAX_RETRIES:  # atingiu o limite de tentativas
+                        print(f"\nTimeout reached ({retries}/{MAX_RETRIES}), resending packet...") # Timeout reached, resend the packet
+                        if retries > MAX_RETRIES:  # Reached the limit of retries
                             print("Maximum number of retries reached, connection lost.")
                             break  
-        else:
+        else: # Window lack of packets, there is nothing more to send
             print("\nWINDOW PACKETS: ",window_data)
-            flag = False
-            break               
-  
+            flag = False # Set the operation of sending to false  
 
 print(f"Total Size of packages Sent: {data_size}")
 print("All packets sent!\nClosing Socket")       
-packet = ''
-packet = packet.encode()
-sock.send(packet)    
+packet = '' # Empty String to be send
+packet = packet.encode() # Transform packet to bytes
+sock.send(packet) # Send packet
 print("Signal to Close connection sent!") 
 time.sleep(1)
 sock.close() 
