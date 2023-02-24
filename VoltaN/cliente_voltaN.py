dfc918f1d5b317b6393a6b102ca854b0c2693784
gabdepa <gabriel-r-p-p@hotmail.com>
2023-02-24 18:17:21 -0300

    Back-N Implemented

--- VoltaN/cliente_voltaN.py
+++ VoltaN/cliente_voltaN.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 # Tamanho do buffer
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 512
 
 # Endereço e porta do servidor
 SERVER_ADDRESS = ('localhost', 5000)
@@ -15,55 +15,92 @@ while True:
     try:
         # Tenta se conectar ao servidor
         sock.connect(SERVER_ADDRESS)
-        print("=> Conncection Established\n")
+        print("=> Connection Established\n")
         break
+    except ConnectionRefusedError:
+        # Caso a conexão seja recusada pelo servidor, termina o loop e encerra o programa
+        print("=> Connection refused by server")
+        sys.exit(1)
     except BlockingIOError:
         # Se ainda não for possível conectar, espera 1 segundo e tenta novamente
         time.sleep(1)
         pass
     except ConnectionResetError:
-        print("Conexão encerrada pelo servidor.")
+        print("=> Connection closed by server")
 
+MAX_RETRIES = 3
+TIMEOUT = 30  # 30 segundos, meio minuto
+window_size = 3
+max_sequence = 7
+last_seq_num = seq_num = -1
+start_time = time.time()  # tempo de início do envio dos pacotes
+retries = 0
+data_size = 0
+window_data = [] # List that hold the window data
 
 # Abertura do arquivo
 filename = "./testMessage.txt"
-seq_num = 0
-
 with open(filename, 'rb') as f:
-    data = f.read(BUFFER_SIZE) # Leitura do arquivo em pedaços de BUFFER_SIZE
-    
-    while data: # Enquanto houver dados a serem enviados
-        try:
-            packet = str(seq_num).encode() + b':' + data # Empacotamento dos dados com o número de sequência
-            while True: # Espera pela confirmação de recebimento do servidor
-                sock.settimeout(5) # Define o timeout como 2 segundo para que o loop possa ser interrompido para reenviar pacotes
-                try:
-                    print("\n=> Enviando dados:\n", data.decode())
-                    sock.send(packet) # Envio dos dados         
-                    confirmation = sock.recv(BUFFER_SIZE) # Armazena a mensagem recebida do servidor
-                    if confirmation == str(seq_num).encode() + b':ACK':
-                        print("\n=> Confirmação recebida: ", confirmation.decode())
-                        last_seq_num = seq_num # Atualiza o último número de sequência confirmado
-                        seq_num+=1 # Incrementa o número de sequência
-                        break # Confirmação recebida, interrompe o loop
-                    elif not confirmation:
-                        print("Conexão encerrada pelo servidor.")
-                        break
-                except sock.timeout:
-                    print("\nTimeout atingido, reenviando pacote...") # Timeout atingido, reenvia o pacote
-        except BlockingIOError:
-            print("Nenhuma mensagem disponível!") # Nenhuma mensagem disponível no momento
-            time.sleep(1)
-            pass
+    while (time.time() - start_time) < TIMEOUT:  # Enquanto houver dados a serem enviados e o tempo limite não foi atingido
+        
+        for i in range(window_size):
+            data = str(f.read(BUFFER_SIZE))
+            if (data == "b''"):
+                # Send packet indicating the sequence has finished
+                packet = ''
+                packet = packet.encode()
+                sock.send(packet)     
+                break 
+            seq_num = int(seq_num) + 1
+            packet = str(seq_num) + " - " + data # Creating packet with sequence number
+            packet = packet.encode()
+            print(f"\n=> Sending Sequence Number: {seq_num} , Element {i} of window, Sending Data:\n {seq_num} - {data}")
+            window_data.append(packet)            
+            data_size += len(packet)              
             
-        data = f.read(BUFFER_SIZE)
-    packet = b""
-    sock.send(packet)
-            
-            
-# Finalização da conexão com o servidor
-time.sleep(1) # Espera 1 segundo antes de fechar a conexão
-sock.close() # Fechamento da conexão
+        if (window_data != []):
+            for packet in window_data: # Envia cada pacote da janela
+                while True: # Espera pela confirmação de recebimento do servidor
+                    try:
+                        
+                        sock.send(packet) # Envio dos dados
+                        message = sock.recv(BUFFER_SIZE) # Armazena a mensagem recebida do servidor
+                        if ":ACK".encode() in message:
+                            print("\n=> Confirmation Received: ", message.decode('utf-8'))
+                            last_seq_num = seq_num
+                            rec_seq_num = message.decode().split(':')[0]
+                            print(f"RECEIVED ACK: {rec_seq_num}")
+                            print("SEQUENCE_NUMBER = ", seq_num)
+                            for i in window_data:
+                                l = i.decode()
+                                if rec_seq_num in l.split(' - ')[0]:
+                                    window_data.remove(i) 
+                            retries = 0  # reseta contador de tentativas após sucesso
+                            print("\n----------WINDOW PACKETS AFTER ACK: ",window_data, "\n")
+                            break # Confirmação recebida, interrompe o loop
+                        else:
+                            print("==> Receipt not confirmed: ", message.decode('utf-8'))
+                            rec_seq_num = message.decode().split(':')[0]
+                            print(rec_seq_num)
+                            for i in window_data:
+                                seq_num = i.decode().split(' - ')[0]
+                                if seq_num == rec_seq_num:
+                                    sock.send(i) 
+                            retries += 0  # reseta contador de tentativas após sucesso
+                            print("\n---------WINDOW PACKETS AFTER NACK: ",window_data, "\n")
 
-       
+                        time.sleep(1)
+                    except socket.timeout:
+                        retries += 1  # incrementa contador de tentativas
+                        print(f"\nTimeout reached ({retries}/{MAX_RETRIES}), resending packet...") # Timeout atingido, reenvia o pacote
+                        if retries > MAX_RETRIES:  # atingiu o limite de tentativas
+                            print("Maximum number of retries reached, connection lost.")
+                            break  
+        else:
+            print("\nWINDOW PACKETS: ",window_data)
+            print(f"Total Size of packages Sent: {data_size}")
+            print("All packets sent!\nClosing Socket")
+            sock.close() 
+            break           
+    
 
--- VoltaN/servidor_voltaN.py
+++ VoltaN/servidor_voltaN.py
@@ -3,7 +3,8 @@ import time
 import os
 
 # Tamanho do buffer
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 512
+SEQ_HEADER = 20
 
 # Endereço e porta do servidor
 SERVER_ADDRESS = ('localhost', 5000)
@@ -30,6 +31,9 @@ while True:
     except ConnectionResetError:
         print("Conexão encerrada pelo servidor.")
 
+max_sequence = 7
+expected_seq_num = 0
+window_size = 3
 
 # Recebimento dos dados
 filename = 'arquivo_recebido.txt'
@@ -38,39 +42,48 @@ with open(filename, 'wb') as f:
     received_data_size = 0
     while True:
         try:
-            # Recebe os dados do cliente, caso exista dados recebidos, eles serão armazenados na variável data.
-            data = conn.recv(BUFFER_SIZE)
+            time.sleep(0.5)
 
-            if data:
-                if b"" in data:
-                    data = data.replace(b"", b"-")
-
-                if b"-" in data:
-                    lines = data.split(b"-")
-                    for line in lines[:-1]:
-                        # O tamanho do dado recebido é adicionado à variável received_data_size
-                        received_data_size += len(line)
-                        # dado recebido é gravado no arquivo criado anteriormente
-                        f.write(line)
-                        # Imprime dados recebidos
-                        print("\n=> Recebendo dados:\n", line.decode())
-                        seq_num, data = line.split(b':', 1)
-                        # envia uma mensagem de confirmação de recebimento ao cliente usando o método send()
-                        conn.send(seq_num + b':ACK')
-                    data = lines[-1]
-
-            else:
-                # Nenhum dado recebido, o loop é interrompido
+            # Recebe os dados do cliente, eles serão armazenados na variável data.
+            data = conn.recv(BUFFER_SIZE*window_size + SEQ_HEADER)
+            data_str = data.decode('utf-8') # Decode the data
+            if data_str != "":
+                message = data_str.split(' - b')[1]
+                # O tamanho do dado recebido é adicionado à variável received_data_size
+                received_data_size += len(data)
+                # dado recebido é gravado no arquivo criado anteriormente
+                f.write(str(message).encode())
+                # Imprime dados recebidos
+                print(f"\n=> Expected Sequence Number: {int(expected_seq_num)} Data Received:\n {data_str}\n")
+                print("MESSAGE: ", message)
+                if b' - ' in data:
+                    seq_num = data_str.split(' - ')[0]
+                    seq_num = int(seq_num)
+                    print("Sequence Number DECODED: ", seq_num)
+                    if seq_num == expected_seq_num:
+                        print(f"Sending {seq_num}:ACK, because {expected_seq_num}={seq_num}")
+                        conn.send(f"{expected_seq_num}:ACK".encode('utf-8'))
+                        if (expected_seq_num < window_size):
+                            expected_seq_num += 1
+                        else: 
+                            expected_seq_num += window_size+1
+                    else:
+                        print(f"Sending {expected_seq_num}:NACK, received: {seq_num} | expected: {expected_seq_num}")
+                        conn.send(f"{expected_seq_num}:NACK".encode('utf-8'))
+            else: # Nenhum dado recebido, o loop é interrompido
+                print(f"\n------> Received END of transmission, closing connection")
                 break
         # Caso ocorra uma exceção do tipo BlockingIOError ou KeyboardInterrupt, o código é direcionado para o bloco except
-        except (BlockingIOError, KeyboardInterrupt):
+        except KeyboardInterrupt:
             # Nenhuma mensagem disponível no momento
-            print("No message at the moment..!")
+            print("KeyBoard Interrupt toggled!")
+            break
+        except BlockingIOError:
+            print("Blocking IO Error raised.")
             pass
 
     # Final dos dados, imprime tamanho total de dados recebidos.
-    print(f"\nTotal received data size: {received_data_size} bytes.")
+    print(f"\nTotal received data size, without Sequence Headers: {received_data_size} bytes.")
 
 # Finalização da conexão com o cliente
-time.sleep(1)
 conn.close()
